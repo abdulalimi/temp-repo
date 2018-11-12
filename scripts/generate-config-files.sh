30358df2306dc946ecca0f755141ec463a3eadba
abdulalimi <abdulalimi@users.noreply.github.com>
2018-11-12 13:22:37 -0600

    Update generate-config-files.sh

--- scripts/generate-config-files.sh
+++ scripts/generate-config-files.sh
@@ -10,3 +10,11 @@ do
     env="$NAME"
     echo "${var//SOURCE/$env}" > ./configs/$env-environment.cfg
 done
+
+var2="`less ./source-registration.properties`"
+
+for NAME in $_env
+do
+    env="$NAME"
+    echo "${var2//SOURCE/$env}" > ./configs/$env-source-registration.cfg
+done
